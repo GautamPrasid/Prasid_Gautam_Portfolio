393fef8e2844edd48fa49795e517ca6329d064ea
GautamPrasid <g.prasud14@gmail.com>
2025-12-22 20:22:02 +0545

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hello, I'm Prasid Gautam! I'm an 18-year-old university student hailing from the scenic city of Pokhara, Nepal. Driven by a passion for technology and an unquenchable curiosity, I’m constantly exploring new skills and pushing the boundaries of my knowledge. Whether it's coding or software development, I'm all about honing my skills and building projects that excite and challenge me.
+          </p>
+          <p className="hero--section-description">
+            Beyond tech, I’m a music enthusiast, a budding cook who loves experimenting with new recipes, and an avid traveler. Traveling allows me to immerse myself in different cultures, savor unique cuisines, and draw inspiration from diverse perspectives. Each adventure broadens my horizon and fuels my drive to create.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Prasid Gautam</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Hello! I'm Prasid Gautam, an 18-year-old university student from Pokhara, Nepal. I am passionate about technology and love exploring new skills.            
+            <br /> I find joy in listening to music, cooking up new dishes, and traveling to experience different cultures. My hobbies extend to coding, where I’m constantly improving my skills in programming and software development.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
